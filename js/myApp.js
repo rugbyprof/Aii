be2bb052de546c6a435fd1a99f9d5b3a22fce936
tsoteen89 <t.osteen89@gmail.com>
2014-05-20 11:25:50 -0500

    added an angular app
    name of app is myApp

---
+++ js/myApp.js
@@ -0,0 +1,53 @@
+//Travis Osteen
+var myApp = angular.module('myApp', ['ngRoute','ui.bootstrap']);
+
+
+myApp.config(function ($routeProvider) {
+
+  $routeProvider.when('/', {
+      controller: 'DropdownCtrl',
+      templateUrl: 'home.html'
+  })
+  .when('/home',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'home.html'
+  })
+  .when('/newPatient',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/newPatient.html'
+  })
+  .when('/accntSettings',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/accntSettings.html'
+  })
+  .when('/logout',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/logout.html'
+  })
+  .when('/messages',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/messages.html'
+  })
+  .when('/invites',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/invites.html'
+  })
+  .when('/careTeam',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/careTeam.html'
+  })
+  .when('/editPatient',
+  {
+      controller: 'DropdownCtrl',
+      templateUrl: 'partials/editPatient.html'
+  })
+  .otherwise({redirectTo: 'home.html'});
+
+});
