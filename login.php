10cc75c15995a469e378146e60716b529a18ebb2
James Rowe <jrrowe55@yahoo.com>
2014-05-21 14:17:16 -0500

    Modified login, register/Added unlinked register.php for possible future use

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$U = new Users('root', 'root');
+	$U = new Users('rootLoginName', 'rootLoginPassword');
 	$encryptedPassword = md5($_POST['password']);
 	if(isset($_POST['login']))
 	{
@@ -17,7 +17,7 @@
 			{
 				echo "Failed to connect!";
 			}
-			$this->db = mysql_select_db('james');
+			$this->db = mysql_select_db('databaseName');
 		}
 		//Moves to the home page if username and password are correct
 		public function login($username, $password)
@@ -69,36 +69,7 @@
 	
 	<body>
 <!-- Header -->
-<div id="top-nav" class="navbar navbar-inverse navbar-static-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-toggle"></span>
-      </button>
-      <a class="navbar-brand" href="#">Control Panel</a>
-    </div>
-    <div class="navbar-collapse collapse">
-		
-      <ul class="nav navbar-nav navbar-right">
-	  <ul class="nav nav-justified " style="width:15px">
-			<li ><a href="#" ><i class="glyphicon glyphicon-cog"></i></a></li>
-			<li><a href="#"><i class="glyphicon glyphicon-heart"></i></a></li>
-			<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-comment"></i><span class="count">3</span></a><ul class="dropdown-menu" role="menu"><li><a href="#">1. Is there a way..</a></li><li><a href="#">2. Hello, admin. I would..</a></li><li><a href="#"><strong>All messages</strong></a></li></ul></li>
-			<li><a href="#"><i class="glyphicon glyphicon-user"></i></a></li>
-			<li><a title="Add Widget" data-toggle="modal" href="#addWidgetModal"><span class="glyphicon glyphicon-plus-sign"></span></a></li>
-		</ul> 
-        <li class="dropdown">
-          <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#">
-            <i class="glyphicon glyphicon-user"></i> Admin <span class="caret"></span></a>
-          <ul id="g-account-menu" class="dropdown-menu" role="menu">
-            <li><a href="#">My Profile</a></li>
-            <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Logout</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div><!-- /container -->
-</div>
+
 <!-- /Header -->
 
 <!-- Main -->
@@ -106,39 +77,16 @@
   
   <!-- upper section -->
   <div class="row">
-	<div class="col-md-3">
-      <!-- left -->
-      <a href="#"><strong><i class="glyphicon glyphicon-briefcase"></i> Toolbox</strong></a>
-      <hr>
-      
-      <ul class="nav nav-pills nav-stacked">
-        <li><a href="#"><i class="glyphicon glyphicon-flash"></i> Alerts</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-link"></i> Links</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-book"></i> Books</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-briefcase"></i> Tools</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-time"></i> Real-time</a></li>
-        <li><a href="#"><i class="glyphicon glyphicon-plus"></i> Advanced..</a></li>
-      </ul>
-      
-      <hr>
-      
-  	</div><!-- /span-3 -->
+	
     <div class="col-md-9">
       	
       <!-- column 2 -->	
-      
-     
-       <a href="#"><strong><i class="glyphicon glyphicon-dashboard"></i> My Dashboard</strong></a>  
-            
-       <hr>
-      
 	   <div class="row">
             <!-- center left-->	
          	<div class="col-md-12">
-			  <div class="well">Inbox Messages <span class="badge pull-right">3</span></div>
+			 
 
-              <hr>
+              
               <div class="panel panel-default">
 				<div class="panel-heading">
 				  <div class="panel-title">
