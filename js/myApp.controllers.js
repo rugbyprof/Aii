f388aa1f352df4a69a3d1b3ec39f54d96c09e855
tsoteen89 <fapppo@masons-iphone.gateway.2wire.net>
2014-05-20 20:55:08 -0500

    Date of Birth
    why is this so hard…

--- js/myApp.controllers.js
+++ js/myApp.controllers.js
@@ -6,4 +6,28 @@ controllers.regularController = function($scope) {
   $scope.name = "Anne";
 }
 
+controllers.dateGenController = function($scope) {
+
+  $scope.months = ["January","February","March","April",
+                  "May","June","July","August","September","October",
+                  "November","December"];
+  $scope.days = [];
+  $scope.years = [];
+  $scope.i = 0;
+
+  for($scope.i=0;$scope.i<32;$scope.i++){
+    $scope.days[$scope.i] = $scope.i;
+  }
+
+  $scope.i = 0;
+  $scope.j = 0;
+
+  for($scope.j=1900;$scope.j<2016;$scope.j++){
+    $scope.years[$scope.i] = $scope.j;
+    $scope.i++;
+  }
+
+
+}
+
 myApp.controller(controllers);
