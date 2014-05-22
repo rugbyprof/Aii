d8164167fdaea3dafa1671dff1b92521ff11f7a5
Jokii19 <jokii19>
2014-05-21 23:26:19 -0500

    Implemented dummy db queries

--- careTeam/js/careTeamControllers/ct_patientcontrollers.js
+++ careTeam/js/careTeamControllers/ct_patientcontrollers.js
@@ -2,17 +2,14 @@ var careTeamApp = angular.module('ct_patientcontrollers',[]);
 
 var controllers = {};
 
-controllers.getPatientDataCtrl = function($scope){
+controllers.getPatientDataCtrl = function($scope, $http){
+        
+    $http.get("backend/patientData.php")
+    .then(function(res){
+        $scope.patients = res.data;
+    });
     
-    $scope.patients = [
-        
-        { name: 'Mando Garcia', id: '123456780', careStatus: 'Active', ear: 'Left Ear' },
-        { name: 'Travis Osteen', id: '123456781', careStatus: 'Active', ear: 'Left Ear' },
-        { name: 'James Rowe', id: '123456782', careStatus: 'Active', ear: 'Right Ear' },
-        { name: 'Anne Lam', id: '123456783', careStatus: 'Active', ear: 'Right Ear' },
-        { name: 'Sanan Aamir', id: '123456784', careStatus: 'Active', ear: 'Left Ear' }
-        
-    ];
+    
        
 }
 
